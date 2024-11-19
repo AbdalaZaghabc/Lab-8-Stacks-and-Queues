3f59b6ff4c079bd871497fd724969bb5d0bfdfe2
azaghabo4834 <azaghabo4834@HCC51221>
2024-11-19 13:17:18 -0500

    Add project files.

---
+++ Project1/azaghabo_Lab8.cpp
@@ -0,0 +1,35 @@
+//Abdala Zaghab
+//CellPhone kiosk 
+//11/21/2024
+
+#include<iostream>
+#include<iomanip>
+#include<fstream>
+#include<vector>
+#include<stack>
+#include<queue>
+using namespace std;
+
+//Constants
+
+//Structures
+
+//Function Prototypes
+void displayTitle();
+
+//Beginning of the program
+int main()
+{
+	displayTitle();
+
+	cout << "\n" << endl;
+	system("pause");
+	return 0;
+}
+
+void displayTitle()
+{
+	cout << right << setw(50) << "CMSY 171 Lab 8" << endl;
+	cout << "Copyright 2024 - Howard Community College All rights reservedl Unauthorized duplication prohibited." << endl << endl;
+	cout << right << setw(65) << "Welcome to CMSY-171 Cell Phone Purchase Program" << endl;
+}
